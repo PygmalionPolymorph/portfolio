6e9f19098c0b4c4bbe79bf2e6a37da077dd74d97
PygmalionPolymorph <pygmalionpolymorph@gmail.com>
2019-05-15 15:23:31 +0200

    Move artist page to contentful

--- src/pagecomponents/artist/index.jsx
+++ src/pagecomponents/artist/index.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
-
-import afloat from '../../assets/artist/afloat.jpg';
-import haunting from '../../assets/artist/haunting.jpg';
-import neondemon from '../../assets/artist/neondemon.jpg';
-import ballerina from '../../assets/artist/ballerina.jpg';
-import figure from '../../assets/artist/figure.jpg';
-import porcelainii from '../../assets/artist/porcelainii.jpg';
-import tetris from '../../assets/artist/tetris.jpg';
+import Img from 'gatsby-image';
 
 import { Gallery } from '../../components/gallery';
 
@@ -14,60 +7,25 @@ import { LinkBar } from '../../components/linkBar';
 import './styles.styl';
 
 const ImageDetail = image => (
-  <img
-    alt={`${image.name}: ${image.alt}`}
-    src={image.src}
+  <Img
     className="gallery__active"
+    fluid={image.src}
+    alt={`${image.title}: ${image.description}`}
+    imgStyle={{
+      objectFit: 'contain',
+    }}
   />
 );
 
 const ImageListItem = (image, i) => (
-  <img
+  <Img
+    fixed={image.thumb}
     alt={`Painting #${i}: ${image.title}`}
     className="gallery__list-image"
-    src={image.src}
   />
 );
 
-const images = [
-  {
-    title: 'Figure',
-    src: figure,
-    alt: 'A woman hangs from a rope, holding her body with her hands and feet, her back forming a tear shape.',
-  },
-  {
-    title: 'Ballerina',
-    src: ballerina,
-    alt: 'Black and white painting of a dancing ballerina. She is standing on one toe and her eyes are closed in ecstasy.',
-  },
-  {
-    title: 'Tetris',
-    src: tetris,
-    alt: 'A naked woman lying upside down, her crotch exposed. Where her genitalia would be, there is an empty rectangle with a tetris bar close to it.',
-  },
-  {
-    title: 'Porcelain II',
-    src: porcelainii,
-    alt: 'A pencil portrait of a woman with short hair and a sharp eyeline. Her expression is between shocked and curious.',
-  },
-  {
-    title: 'Afloat',
-    src: afloat,
-    alt: 'Black and white painting of a bare-chested woman in a foggy field. Her head is bent backwards and her eyes closed. She is surrounded by white lights.',
-  },
-  {
-    title: 'The Neon Demon',
-    src: neondemon,
-    alt: 'Neon-colored portrait of a woman looking at the viewer. Her head is inside a pink glowing triangle. Her throat seems to be cut with blood running down her throat.',
-  },
-  {
-    title: 'Haunting',
-    src: haunting,
-    alt: 'A woman in a long red gown is walking towards the viewer. Under the coat she wears nothing, so you can see her bare body. The atmosphere is dark and haunting.',
-  },
-];
-
-export const Artist = ({ inverse, active }) => (
+export const Artist = ({ images, inverse, active }) => (
   <main className={`artist ${inverse ? 'inverse' : null}`}>
     <Gallery
       active={active}
--- src/pages/artist.jsx
+++ src/pages/artist.jsx
@@ -1,22 +1,62 @@
-import React from 'react';
 import { stream } from 'flyd';
-import { useStreamState } from '../utils/stream';
+import { graphql } from 'gatsby';
+import React from 'react';
+import {
+  compose, map, head, path,
+} from 'ramda';
 
 import SEO from '../components/seo';
 import { Artist } from '../pagecomponents/artist/index';
 
 import '../style/styles.styl';
+import { useStreamState } from '../utils/stream';
+
+const gallery = compose(
+  map(n => ({
+    title: n.title,
+    description: n.photo.description,
+    thumb: n.photo.thumb,
+    src: n.photo.src,
+  })),
+  path(['node', 'items']),
+  head,
+  path(['allContentfulCategory', 'edges']),
+);
+
+export const query = graphql`
+{
+  allContentfulCategory(filter: { title: { eq: "artist" }}) {
+    edges {
+      node {
+        items {
+          title
+          photo {
+            description
+            src: fluid(maxWidth: 1000, quality:100) {
+              ...GatsbyContentfulFluid
+            }
+            thumb: fixed(height: 150, width: 150) {
+              ...GatsbyContentfulFixed
+            } 
+          }
+        }
+      }
+    }
+  }
+}
+`;
 
 const active = stream(0);
 
-export default () => {
+export default ({ data }) => {
+  console.log(data);
   useStreamState(active);
 
   return (
     <section className="page">
       <SEO title="Artist" keywords={['portfolio', 'homepage', 'artist']} />
-      <Artist active={active} />
-      <Artist active={active} inverse />
+      <Artist images={gallery(data)} active={active} />
+      <Artist images={gallery(data)} active={active} inverse />
     </section>
   );
 };
